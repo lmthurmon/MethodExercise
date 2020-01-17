1f18f7e7c4b6de814652495053f204a2451148d2
Landon Thurmon <landonthurmon1@gmail.com>
2020-01-17 13:52:54 -0600

    overloading methods/notes

--- Method Exercise/Method Exercise/Program.cs
+++ Method Exercise/Method Exercise/Program.cs
@@ -19,10 +19,12 @@ namespace Method_Exercise
                  $"when listening to {bandName} with {animalName} .");
                  */
 
-            Console.WriteLine(FindSum(2, 4));
+            Console.WriteLine(FindSum(2.2m, 4.6m));
             Console.WriteLine(FindMultiply(10, 2));
             Console.WriteLine(FindSum(1, 1) * FindMultiply(10, 2));
             Console.WriteLine(MyMoney(2.85, 3.4, 5.2));
+            Console.WriteLine(GetMax() * FindSum(2, 2));
+            FindSum(1, 1, true);
         }
 
         public static int FindSum(int x, int y)
@@ -30,6 +32,8 @@ namespace Method_Exercise
             return x + y;
         }
 
+
+        //method can use same name and return type but must have different signature in parameters(value type or number of args)
         public static int FindMultiply(int x, int y)
         {
             return x * y;
@@ -37,11 +41,54 @@ namespace Method_Exercise
         }
         public static double MyMoney(double a, double b, double c)
         {
-            return (a * b) / c; 
-                }
+            return (a * b) / c;
+        }
+        //can remove retrun type and put Console.Writeline in method by putting public static void(replace return type with void)
+
+        public static int GetMax()
+        {
+            Console.WriteLine("Enter three numbers to find the greatest value");
+            Console.WriteLine("First Number:");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Second Number");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Third Number");
+            int thirdNumber = Convert.ToInt32(Console.ReadLine());
 
 
-      
+            if (firstNumber > secondNumber)
+                return firstNumber;
+
+            else if (thirdNumber > secondNumber)
+                return thirdNumber;
+            else
+                return secondNumber;
+
+
+        }
+        //method overload example of FindSum
+        public static decimal FindSum(decimal x, decimal y)
+        {
+            return x + y;
+        }
+
+        public static void FindSum(int x, int y, bool z)
+
+        {
+            if (z == true)
+            {
+                Console.WriteLine(x + y + " Dollars");
+            }
+            else
+            {
+                Console.WriteLine(x + y + "Dollar");
+            } 
+
+
+        }
+
+
+
     }
-    
 }
+
