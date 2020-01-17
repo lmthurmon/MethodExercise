b4d50b141b6c8ead855ed9fbe406d46a60361262
Landon Thurmon <landonthurmon1@gmail.com>
2020-01-17 10:44:57 -0600

    readline madlib

--- Method Exercise/Method Exercise/Program.cs
+++ Method Exercise/Method Exercise/Program.cs
@@ -6,9 +6,17 @@ namespace Method_Exercise
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is your name?");
+            var yourName =Console.ReadLine();
             Console.WriteLine("What is your favorite color?");
            var favoriteColor = Console.ReadLine();
-            Console.WriteLine($"Your favorite color is {favoriteColor}");
+            Console.WriteLine("Name an animal");
+            var animalName = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            var bandName = Console.ReadLine();
+
+            Console.WriteLine($"Your name is {yourName} , and you turn the color {favoriteColor} " +
+                $"when listening to {bandName} with {animalName}");
 
 
         }
