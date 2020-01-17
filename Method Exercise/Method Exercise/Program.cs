2556bc215987d850ca0403b8e83ff92a643b700b
Landon Thurmon <landonthurmon1@gmail.com>
2020-01-17 12:09:08 -0600

    added more returns

--- Method Exercise/Method Exercise/Program.cs
+++ Method Exercise/Method Exercise/Program.cs
@@ -6,19 +6,42 @@ namespace Method_Exercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your name?");
-            var yourName =Console.ReadLine();
-            Console.WriteLine("What is your favorite color?");
-           var favoriteColor = Console.ReadLine();
-            Console.WriteLine("Name an animal");
-            var animalName = Console.ReadLine();
-            Console.WriteLine("What is your favorite band?");
-            var bandName = Console.ReadLine();
+            /* Console.WriteLine("What is your name?");
+             var yourName =Console.ReadLine();
+             Console.WriteLine("What is your favorite color?");
+            var favoriteColor = Console.ReadLine();
+             Console.WriteLine("Name an animal");
+             var animalName = Console.ReadLine();
+             Console.WriteLine("What is your favorite band?");
+             var bandName = Console.ReadLine();
 
-            Console.WriteLine($"Your name is {yourName} , and you turn the color {favoriteColor} " +
-                $"when listening to {bandName} with {animalName}");
+             Console.WriteLine($"Your name is {yourName} , and you turn the color {favoriteColor} " +
+                 $"when listening to {bandName} with {animalName} .");
+                 */
 
+            Console.WriteLine(FindSum(2, 4));
+            Console.WriteLine(FindMultiply(10, 2));
+            Console.WriteLine(FindSum(1, 1) * FindMultiply(10, 2));
+            Console.WriteLine(MyMoney(2.85, 3.4, 5.2));
+        }
+
+        public static int FindSum(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int FindMultiply(int x, int y)
+        {
+            return x * y;
 
         }
+        public static double MyMoney(double a, double b, double c)
+        {
+            return (a * b) / c; 
+                }
+
+
+      
     }
+    
 }
