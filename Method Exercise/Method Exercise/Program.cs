e32b1fad6408ac40bfa6f48161488ea606f60ca2
Landon Thurmon <landonthurmon1@gmail.com>
2020-01-17 10:34:52 -0600

    project start and color line

---
+++ Method Exercise/Method Exercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Method_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite color?");
+           var favoriteColor = Console.ReadLine();
+            Console.WriteLine($"Your favorite color is {favoriteColor}");
+
+
+        }
+    }
+}
